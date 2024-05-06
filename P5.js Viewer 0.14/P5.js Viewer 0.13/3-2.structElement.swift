25129ab37a48745b8092717e793a8ca5fb15d7eb
Jiaqi Yi <70934733+Typemaster32@users.noreply.github.com>
2024-05-05 21:41:03 -0400

    tidied the files

--- P5.js Viewer 0.14/P5.js Viewer 0.13/3-2.structElement.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-struct ElementModel: Identifiable, Codable {
-    var id = UUID() // Conforming to Identifiable
-    let title: String
-    let author: String
-    let folderURL: String //This should actually be a URL
-    let p5id: String
-    let createdAt: Date
-}
